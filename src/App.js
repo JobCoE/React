eb53a6cbad3347c30d9570719df0664a1edba1ee
JoBCoE <jobzaza67@gmail.com>
2018-09-13 10:21:24 +0700

    second checkpoint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharacterCard from './CharacterCrad';
+
 class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
- </div>
- );
- }
+  render() {
+    return (
+      <div className="App">
+        <CharacterCard value="h" />
+        <CharacterCard value="i" />
+       
+      </div>
+    );
+  }
 }
 export default App;
 
