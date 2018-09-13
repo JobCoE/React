cea28c98a19a2925c36d47ec4fba686aba1ffdcb
JoBCoE <jobzaza67@gmail.com>
2018-09-13 12:50:21 +0700

    7th commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div>
         {
           //Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
-          <WordCard value="sirawit"/>
+          <WordCard value="hello"/>
         }
       </div>
     );
